fffafe54d92265841b1572de86130586fa841747
Hardik Bansal <hardik.bansal@hashedin.com>
2021-05-17 16:18:31 +0530

    Added Load State to Order Config

--- src/components/landingPages/OrderConfigPage/OrderConfigPage.tsx
+++ src/components/landingPages/OrderConfigPage/OrderConfigPage.tsx
@@ -12,6 +12,8 @@ import RoundButton from "../../widgets/RoundButton/RoundButton";
 import { RoundButtonModel } from "../../widgets/RoundButton/RoundButtonModel";
 import { Link } from "react-router-dom";
 import { OrderCompleteModel, OrderCompleteProps } from "../OrderCompletePage/OrderCompleteModel";
+import ErrorState from "../../widgets/ErrorState/ErrorState";
+import LoadingState from "../../widgets/LoadingState/LoadingState";
 
 const OrderConfigPage = () => {
     const [carConfig, setCarConfig] = useState<CarConfiguration>();
@@ -20,16 +22,20 @@ const OrderConfigPage = () => {
     const [paintIndex, setPaintIndex] = useState(0);
     const [wheelIndex, setWheelIndex] = useState(0);
     const [autopilot, setAutopilot] = useState(false)
+    const [errored, setErrored] = useState(false)
 
     useEffect(() => {
         const getCarConfiguration = async () => {
             let response = await fetch('https://tesla-app-server.herokuapp.com/models/' + modelId + '/configure');
+            console.log('ffffffff' + response)
             if (!response.ok) {
+                setErrored(true)
                 throw new Error(response.statusText)
             }
             setCarConfig(await response.json());
         }
-        getCarConfiguration();
+        setTimeout(() =>
+            getCarConfiguration().catch(() => setErrored(true)), 1000)
     }, [modelId]);
 
     console.log(carConfig)
@@ -93,110 +99,115 @@ const OrderConfigPage = () => {
                     ></img>
                 </div>
 
-                <div className="order-config-info"
-                    style={{
-                        width: desktopMode ? '50vw' : '100vw'
-                    }}>
-                    <div className="order-config-car-name">{carConfig?.displayName}</div>
+                {errored && <div className="order-config-error"><ErrorState /></div>}
 
-                    <div className="order-config-three-row-wrapper">
-                        <ThreeRowData
-                            estimatedRange={carConfig?.variants[variantIndex].range + ' mi'}
-                            topSpeed={carConfig?.variants[variantIndex].topSpeed + ' mph'}
-                            accelerationTime={carConfig?.variants[variantIndex].acceleration.time + ' sec'} />
-                    </div>
+                {!errored && carConfig == undefined && <div className="order-config-error"><LoadingState /></div>}
 
-                    <div className="order-confid-variant-container">
-                        {
-                            carConfig?.variants.map((variant, index) => {
-                                const roundTwoButton: RoundButtonTwoValuesModel = {
-                                    value1: variant.variantName,
-                                    value2: '$ ' + variant.price,
-                                    borderColor: '#787878',
-                                    borderColorSelected: '#097BE4',
-                                    selected: variantIndex == index,
-                                    onClickFunction: () => setVariantIndex(index)
-                                }
-                                return <RoundButtonTwoValues roundButtonTwoValuesModel={roundTwoButton}></RoundButtonTwoValues>
-                            })
-                        }
-                    </div>
+                {!errored && carConfig != undefined &&
+                    <div className="order-config-info"
+                        style={{
+                            width: desktopMode ? '50vw' : '100vw'
+                        }}>
+                        <div className="order-config-car-name">{carConfig?.displayName}</div>
 
-                    <div className="order-config-property-static">Paint</div>
-                    <div className='order-config-property-list'>
-                        {carConfig?.paints.map((paint, index) => {
-                            return (
-                                <div>
-                                    <input type="image" className='circle-small'
-                                        style={{ border: index == paintIndex ? '3px solid #097BE4' : '0' }}
-                                        src={paint.src} onClick={() => setPaintIndex(index)} />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className='order-config-property-selection'>
-                        <div className="property-selected-name">{carConfig?.paints[paintIndex].name} Multi-Coat</div>
-                        <div className="property-selected-price">{carConfig?.paints[paintIndex].default ? 'Included' : '$' + carConfig?.paints[paintIndex].price}</div>
-                    </div>
-
-
-                    <div className='order-config-property-static'>Wheels</div>
-                    <div className='order-config-property-list'>
-                        {carConfig?.wheels.map((wheel, index) => {
-                            return (
-                                <div>
-                                    <input type="image" src={wheel.src} className='circle-big'
-                                        style={{ border: index == wheelIndex ? '3px solid #097BE4' : '0' }}
-                                        onClick={() => setWheelIndex(index)} />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className='order-config-property-selection'>
-                        <div className="property-selected-name">{carConfig?.wheels[wheelIndex].name}</div>
-                        <div className="property-selected-price">{carConfig?.wheels[wheelIndex].default ? 'Included' : '$' + carConfig?.wheels[wheelIndex].price}</div>
-                    </div>
-
-                    {carConfig?.selfDriving.available ?
-                        <div className='order-config-self-drive'>
-                            <div className="order-config-property-static">Full Self Driving Capabilities</div>
-                            <div className="order-config-self-drive-value-container">
-                                <label className="order-config-self-drive-value">Navigate on Autopilot</label>
-                                <label className="order-config-self-drive-value">Auto-lane Change</label>
-                                <label className="order-config-self-drive-value">Autopark</label>
-                                <label className="order-config-self-drive-value">Summon</label>
-                                <label className="order-config-self-drive-value">Full Self Driving Computer</label>
-                            </div>
-
-                            <div className="order-config-add-button-container">
-                                <RoundButton roundButtonModel={addSelfDriveButton}></RoundButton>
-                                {/* <button onClick={() => setAutopilot(!autopilot)}> {autopilot ? 'Remove' : 'Add'}</button> */}
-
-                                {autopilot && <div className='order-config-self-drive-selection'>
-                                    <div className="property-selected-name">Self Drive</div>
-                                    <div className="property-selected-price">{'$' + carConfig.selfDriving.price}</div>
-                                </div>}
-                            </div>
+                        <div className="order-config-three-row-wrapper">
+                            <ThreeRowData
+                                estimatedRange={carConfig?.variants[variantIndex].range + ' mi'}
+                                topSpeed={carConfig?.variants[variantIndex].topSpeed + ' mph'}
+                                accelerationTime={carConfig?.variants[variantIndex].acceleration.time + ' sec'} />
                         </div>
-                        :
-                        ''}
 
-                    <div className="order-config-order-static">
-                        <div className="order-config-property-static">Order your {carConfig?.displayName}</div>
-                        <div className="property-selected-price">Estimated Deliver: 6-8 weeks</div>
-                    </div>
-
-                    <Link to={{
-                        pathname: '/orderComplete',
-                        state: {
-                            choosenModel: 'abcdefg'
-                        }
-                    }}>
-                        <div className="order-config-order-button-container">
-                            <RoundButton roundButtonModel={orderButton}></RoundButton>
+                        <div className="order-confid-variant-container">
+                            {
+                                carConfig?.variants.map((variant, index) => {
+                                    const roundTwoButton: RoundButtonTwoValuesModel = {
+                                        value1: variant.variantName,
+                                        value2: '$ ' + variant.price,
+                                        borderColor: '#787878',
+                                        borderColorSelected: '#097BE4',
+                                        selected: variantIndex == index,
+                                        onClickFunction: () => setVariantIndex(index)
+                                    }
+                                    return <RoundButtonTwoValues roundButtonTwoValuesModel={roundTwoButton}></RoundButtonTwoValues>
+                                })
+                            }
                         </div>
-                    </Link>
-                </div>
+
+                        <div className="order-config-property-static">Paint</div>
+                        <div className='order-config-property-list'>
+                            {carConfig?.paints.map((paint, index) => {
+                                return (
+                                    <div>
+                                        <input type="image" className='circle-small'
+                                            style={{ border: index == paintIndex ? '3px solid #097BE4' : '0' }}
+                                            src={paint.src} onClick={() => setPaintIndex(index)} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className='order-config-property-selection'>
+                            <div className="property-selected-name">{carConfig?.paints[paintIndex].name} Multi-Coat</div>
+                            <div className="property-selected-price">{carConfig?.paints[paintIndex].default ? 'Included' : '$' + carConfig?.paints[paintIndex].price}</div>
+                        </div>
+
+
+                        <div className='order-config-property-static'>Wheels</div>
+                        <div className='order-config-property-list'>
+                            {carConfig?.wheels.map((wheel, index) => {
+                                return (
+                                    <div>
+                                        <input type="image" src={wheel.src} className='circle-big'
+                                            style={{ border: index == wheelIndex ? '3px solid #097BE4' : '0' }}
+                                            onClick={() => setWheelIndex(index)} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className='order-config-property-selection'>
+                            <div className="property-selected-name">{carConfig?.wheels[wheelIndex].name}</div>
+                            <div className="property-selected-price">{carConfig?.wheels[wheelIndex].default ? 'Included' : '$' + carConfig?.wheels[wheelIndex].price}</div>
+                        </div>
+
+                        {carConfig?.selfDriving.available ?
+                            <div className='order-config-self-drive'>
+                                <div className="order-config-property-static">Full Self Driving Capabilities</div>
+                                <div className="order-config-self-drive-value-container">
+                                    <label className="order-config-self-drive-value">Navigate on Autopilot</label>
+                                    <label className="order-config-self-drive-value">Auto-lane Change</label>
+                                    <label className="order-config-self-drive-value">Autopark</label>
+                                    <label className="order-config-self-drive-value">Summon</label>
+                                    <label className="order-config-self-drive-value">Full Self Driving Computer</label>
+                                </div>
+
+                                <div className="order-config-add-button-container">
+                                    <RoundButton roundButtonModel={addSelfDriveButton}></RoundButton>
+                                    {/* <button onClick={() => setAutopilot(!autopilot)}> {autopilot ? 'Remove' : 'Add'}</button> */}
+
+                                    {autopilot && <div className='order-config-self-drive-selection'>
+                                        <div className="property-selected-name">Self Drive</div>
+                                        <div className="property-selected-price">{'$' + carConfig.selfDriving.price}</div>
+                                    </div>}
+                                </div>
+                            </div>
+                            :
+                            ''}
+
+                        <div className="order-config-order-static">
+                            <div className="order-config-property-static">Order your {carConfig?.displayName}</div>
+                            <div className="property-selected-price">Estimated Deliver: 6-8 weeks</div>
+                        </div>
+
+                        <Link to={{
+                            pathname: '/orderComplete',
+                            state: {
+                                choosenModel: 'abcdefg'
+                            }
+                        }}>
+                            <div className="order-config-order-button-container">
+                                <RoundButton roundButtonModel={orderButton}></RoundButton>
+                            </div>
+                        </Link>
+                    </div>}
             </div>
         </div >
     )
